865a6d28186192672b76a514d120a18e70f96d3b
Sankalp-Kalode <sankalpkalode1@gmail.com>
2023-05-04 22:41:57 +0530

    done

---
+++ test.js
@@ -0,0 +1,28 @@
+//document.write("This is an external JS");
+document.getElementById('th').innerHTML = "This is para";
+
+var a1 = 4;
+	var a2 = "This is a variable";
+	document.write(a1);
+
+var age = prompt("What is your age?");
+switch(age)
+{
+
+	case "22":
+	document.write("22 years old.. okay");
+	break;
+
+	case "21":
+	document.write("21 years old.. okay");
+	break;
+
+	case "12":
+	document.write("12 years old.. okay");
+	break;
+
+	default:
+	document.write("default years old.. okay");
+	break;
+
+}

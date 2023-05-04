1a11f48029dce01759d6a8afd177c1853cb5e1f2
Sankalp-Kalode <sankalpkalode1@gmail.com>
2023-05-04 22:30:02 +0530

    b2 updated

---
+++ Calculate.java
@@ -0,0 +1,16 @@
+public class Calculate {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		int c= a+b;
+		System.out.println("a = "+ a);
+		System.out.println("b ="+ b);
+		System.out.println("sum of "+a+" and "+b+" = "+ c);
+
+	}
+
+}
+
+ 

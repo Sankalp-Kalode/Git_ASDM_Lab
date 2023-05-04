7e66021e7a4aff1f3dd6f1b4ea8cafa8bef91621
Sankalp-Kalode <sankalpkalode1@gmail.com>
2023-05-04 21:57:34 +0530

    Done

---
+++ printTable.java
@@ -0,0 +1,16 @@
+public class printTable {
+
+	public static void main(String[] args) {
+		
+		int n = 2;
+		
+		for(int i = 1; i<=10; i++)
+		{
+			System.out.println(n+" * "+i+" = "+n*i);
+			
+		}
+		
+
+	}
+
+}
